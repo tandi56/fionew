a3d884fc52f2f3ff18e0b021efb19dfc6ba1e92e
tandi56 <tandisunjaya56@gmail.com>
2023-12-18 23:49:18 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:figurenew2/screens/homescree.dart';
+import 'package:figurenew2/screens/product_detail_screen.dart';
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import './providers/all_products.dart';
+import './providers/cart.dart';
+import 'screens/cart_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => Cart(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => Products(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'MyShop',
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+        ),
+        home: homescreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+          CartScreen.routeName: (ctx) => CartScreen(),
+        },
+      ),
+    );
+  }
+}
